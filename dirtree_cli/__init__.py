d860adf2ca5aa2782db27af99b4404c9ecc5ab1c
Viachaslau Ryneiski <viachaslau_ryneiski@epam.com>
2021-03-23 10:27:20 +0300

    Add composite main function
    - add main
    - add __init__.py

--- dirtree_cli/__init__.py
+++ dirtree_cli/__init__.py
@@ -1,9 +1,19 @@
-__version__ = "0.0.1"
+"""
+This module imitate work with directories without side effect
+in real file system.
+
+Directories saves in Directory composite (tree like view) and commands
+executes by command dispatcher which translate command to directory function.
+"""
+__version__ = "1.0.0"
 
 
 from .main import main
-
+from . import dir_composite
+from . import command_dispatcher
 
 __all__ = (
-    'main'
+    'main',
+    'dir_composite',
+    'command_dispatcher'
 )
--- dirtree_cli/main.py
+++ dirtree_cli/main.py
@@ -1,2 +1,15 @@
+from .command_dispatcher import CommandDispatcher
+
+
 def main():
-    pass
+    dispatcher = CommandDispatcher()
+    print("RUN DirTree CLI. (Press CTRL+C to quit)")
+    while True:
+        command = input(">>> ")
+        if command:
+            try:
+                result = dispatcher.execute(command)
+                if result:
+                    print(result)
+            except Exception as exc:
+                print(exc)
