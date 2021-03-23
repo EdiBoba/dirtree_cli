9dfd41fde316a02495c5d68c70098393d31f7e34
Viachaslau Ryneiski <viachaslau_ryneiski@epam.com>
2021-03-23 14:29:41 +0300

    Add dispatcher test

---
+++ tests/test_command_dispatcher/test_dispatcher.py
@@ -0,0 +1,22 @@
+import pathlib
+
+import pytest
+
+from dirtree_cli.command_dispatcher.exceptions import UnknownCommandError
+
+
+def test_unknown_command(command_dp):
+    with pytest.raises(UnknownCommandError):
+        command_dp.execute('NOTACOMMAND')
+
+
+def test_execute_from_file(command_dp):
+    file_path = pathlib.Path(__file__).parent.parent / 'commands.txt'
+    command_dp.execute(f"EXEC {file_path}")
+    assert command_dp.execute("list") == f"""\
+{pathlib.Path().cwd()}\r
+\tfoods\r
+\t\tfruits\r
+\t\tgrains\r
+\t\tvegetables\r
+\t\t\tsquash"""
